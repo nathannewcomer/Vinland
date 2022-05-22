ce91e7796155d7b46507cde7ba4da850879d0e0f
Nathan Newcomer <newcomer.62@buckeyemail.osu.edu>
2022-05-22 14:47:35 -0400

    fixing duchy mistakes

--- tools/duchy_maker.py
+++ tools/duchy_maker.py
@@ -5,6 +5,7 @@
 
 import sys
 import sqlite3
+import traceback
 
 if len(sys.argv) < 3:
     print("Usage: (program) (duchy) [province ids]")
@@ -23,25 +24,42 @@ counties = []
 
 # get counties from given province ids
 sql = "SELECT county FROM barony WHERE province = ?"
-for prov_id in prov_ids:
-    result = cursor.execute(sql, (prov_id,))
-    county = result.fetchone()
-    counties.append(str(county))
+try:
+    for prov_id in prov_ids:
+        result = cursor.execute(sql, (prov_id,)).fetchone()
+        county = result[0]
+        assert isinstance(county, str), f"Expected county to be of type string, got {type(county)}"
+        counties.append(county)
 
-# get county capital name from province id
-sql = "SELECT county FROM barony WHERE province = ?"
-county_capital = str(cursor.execute(sql, (capital_id,)).fetchone()[0])
+    print("counties are:", counties)
 
-# insert duchy
-sql = "INSERT INTO duchy VALUES (?, ?, ?, ?, ?)"
-cursor.execute(sql, (duchy_name, 0x000000, 0x000000, None, county_capital))
+    # get county capital name from province id
+    sql = "SELECT county FROM barony WHERE province = ?"
+    result = cursor.execute(sql, (capital_id,)).fetchone()
+    county_capital = result[0]
+    assert isinstance(county_capital, str), f"Expected county_capital to be of type string, got {type(county_capital)}"
 
-# add duchy to associated county column
-for county in counties:
-    sql = "UPDATE county SET duchy = ? WHERE name = ?"
-    cursor.execute(sql, (duchy_name, county))
+    print("Capital is:", county_capital)
 
-connection.commit()
+    # insert duchy
+    sql = "INSERT INTO duchy VALUES (?, ?, ?, ?, ?)"
+    cursor.execute(sql, (duchy_name, 0x000000, 0x000000, None, county_capital))
+
+    print("Added duchy", duchy_name, "to database.")
+
+    # add duchy to associated county column
+    for county in counties:
+        sql = "UPDATE county SET duchy = ? WHERE name = ?"
+        cursor.execute(sql, (duchy_name, county))
+
+    print("Associated duchy to counties.")
+    connection.commit()
+    print("Success!")
+except sqlite3.Error as er:
+    print('SQLite error: %s' % (' '.join(er.args)))
+    print("Exception class is: ", er.__class__)
+    print('SQLite traceback: ')
+    exc_type, exc_value, exc_tb = sys.exc_info()
+    print(traceback.format_exception(exc_type, exc_value, exc_tb))
+
 connection.close()
-
-print("success!")
